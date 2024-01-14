82ed713bd9a7d961c100ec177f22c4886017fac2
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-01-14 15:50:30 -0300

    set form field data whith dynamic name

--- react-formik/src/forms/pages/RegisterPage.tsx
+++ react-formik/src/forms/pages/RegisterPage.tsx
@@ -1,13 +1,55 @@
+import { ChangeEvent, useState } from 'react';
 import '../styles/styles.css';
+
 export const RegisterPage = () => {
+  const [registerData, setRegisterData] = useState({
+    name: '',
+    email: '',
+    password1: '',
+    password2: '',
+  });
+
+  const handleOnChance = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    setRegisterData({ ...registerData, [name]: value });
+    // console.log(e.target.value);
+  };
+
+  const { name, email, password1, password2 } = registerData;
+  console.log(registerData);
+
   return (
     <div>
       <h1> Register Page</h1>
       <form>
-        <input type="text" placeholder="name" />
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password1" />
-        <input type="password" placeholder="repeat password2" />
+        <input
+          value={name}
+          name="name"
+          type="text"
+          placeholder="name"
+          onChange={handleOnChance}
+        />
+        <input
+          name="email"
+          value={email}
+          type="email"
+          placeholder="email"
+          onChange={handleOnChance}
+        />
+        <input
+          name="password1"
+          value={password1}
+          type="password"
+          placeholder="password1"
+          onChange={handleOnChance}
+        />
+        <input
+          name="password2"
+          value={password2}
+          type="password"
+          placeholder="repeat password2"
+          onChange={handleOnChance}
+        />
         <button type="submit">Create</button>
       </form>
     </div>
