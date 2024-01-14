59ab948dc2ca6edcb5a7d0c34feb6f200994f739
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-01-14 18:44:44 -0300

    intalled formik and use hook useFormik

--- react-formik/src/forms/pages/FormikBasicPage.tsx
+++ react-formik/src/forms/pages/FormikBasicPage.tsx
@@ -1,16 +1,59 @@
+import { useFormik } from 'formik';
 import '../styles/styles.css';
 
 export const FormikBasicPage = () => {
+  const { handleSubmit, handleChange, values } = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    //  async onSubmit(values) {
+    //    console.log(values);
+    //  },
+  });
+
+  const { firstName, lastName, email } = values;
+
   return (
     <div>
       <h1>Formik Basic tutorial</h1>
-      <form noValidate>
+      <form noValidate onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name</label>
-        <input type="text" name="firstName" placeholder="First Name" />
+        <input
+          type="text"
+          name="firstName"
+          value={firstName}
+          placeholder="First Name"
+          onChange={handleChange}
+        />
+        <span>First name i s required </span>
+
         <label htmlFor="lastName">Last Name</label>
-        <input type="text" name="lastName" placeholder="Last Name" />
+        <input
+          type="text"
+          name="lastName"
+          value={lastName}
+          placeholder="Last Name"
+          onChange={handleChange}
+        />
+        <span>Last name i s required </span>
+
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" placeholder="Email" />
+        <input
+          type="email"
+          name="email"
+          value={email}
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+        <span>Email is required </span>
+        <span>Check for an valid email format </span>
+
         <button type="submit">Submit</button>
       </form>
     </div>
